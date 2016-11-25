923615df4bc80e69fbcc0636666b3f5377192c21
Luis Gerardo <luigii_1506@hotmail.com>
2016-11-25 11:21:02 -0800

    MaterialUI-Sidenav-Working

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AccountsUI from '../AccountsUI.jsx';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import Navbar from './Navbar.jsx';
 
 export const MainLayout = ({content}) => (
   <div>
-  <nav>
-    <div className="nav-wrapper">
-      <a href="/" className="brand-logo">RutaTj.io</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="/rutas">Rutas</a></li>
-        <li><a href="/about">Nosotros</a></li>
-        <li><div className="divider"></div></li>
-        <li><a className="waves-effect" href="#!">Contacto</a></li>
-        <AccountsUI />
-      </ul>
-    </div>
-  </nav>
-
+    <Navbar />
     <main>
-      <MuiThemeProvider>
-        {content}
-      </MuiThemeProvider>
+      {content}
     </main>
   </div>
 )
